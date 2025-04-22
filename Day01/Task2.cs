5311fbd091d02a4e14fc0cf3ac7fe357906d1d5c
Ahmed Abosalem <66332650+ahmedabosalem99@users.noreply.github.com>
2025-04-22 13:24:47 +0200

    Add files via upload

---
+++ Day01/Task2.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public interface IAreaCalculable
+    {
+        double Area();
+    }
+
+    public interface IVolumeCalculable : IAreaCalculable
+    {
+        double Volume();
+    }
+
+    public class Rectangle : IAreaCalculable
+    {
+        public Rectangle()
+        {
+        }
+
+        public double Height { get; set; }
+        public double Width { get; set; }
+
+        public double Area() => Height * Width;
+    }
+
+    public class Circle : IAreaCalculable
+    {
+        public double Radius { get; set; }
+
+        public double Area() => Radius* Radius * Math.PI;
+
+    }
+
+    public class AreaCalculator
+    {
+
+        public double TotalArea(IEnumerable<IAreaCalculable> shapes) => shapes.Sum(s => s.Area());
+        
+    }
+
+    public class Square : IAreaCalculable
+    {
+        public double SideLength { get;  set; }
+
+        public double Area() => SideLength * SideLength;
+
+    }
+
+    public class Triangle : IAreaCalculable
+    {
+        public double Base { get; set; }
+        public double Height { get; set; }
+
+        public double Area() => (Base * Height) / 2;
+    }
+
+    public class Cube : IVolumeCalculable
+    {
+        public double SideLength { get; set; }
+
+        public double Area() => 6 * SideLength * SideLength;
+        public double Volume() => SideLength * SideLength * SideLength;
+    }
+
+}
---
+++ Day01/Task3.cs
@@ -0,0 +1,43 @@
+﻿using Day1;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public abstract class ShapeBase
+    {
+        public double dimensionOne;
+
+        public abstract void SetDimensionOne(double value);
+
+    }
+
+    public class Rectangle : ShapeBase
+    {
+        public double dimensionTwo;
+
+        public override void SetDimensionOne(double value)
+        {
+            dimensionOne = value;
+        }
+        public  void SetDimensionTwo(double value)
+        {
+            dimensionTwo = value;
+        }
+    }
+
+    public class Square : ShapeBase
+    {
+        public override void SetDimensionOne(double value)
+        {
+            dimensionOne = value;
+        }
+    }
+
+}
+
+
